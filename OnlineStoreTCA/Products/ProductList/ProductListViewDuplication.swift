3d14987926b34c82b47b98b0f361397cc9094498
Daniel Torres <danstorre@gmail.com>
2023-01-24 10:10:46 -0400

    move `ProductListView` logic to `ProductListViewDuplication` to separate view's from navigation logic.

---
+++ OnlineStoreTCA/Products/ProductList/ProductListViewDuplication.swift
@@ -0,0 +1,58 @@
+//
+//  ProductListView.swift
+//  OnlineStoreTCA
+//
+//  Created by Pedro Rojas on 17/08/22.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct ProductListViewDuplication: View {
+    let store: Store<ProductListDomain.State,ProductListDomain.Action>
+    
+    var body: some View {
+        WithViewStore(self.store) { viewStore in
+            Group {
+                if viewStore.isLoading {
+                    ProgressView()
+                        .frame(width: 100, height: 100)
+                } else if viewStore.shouldShowError {
+                    ErrorView(
+                        message: "Oops, we couldn't fetch product list",
+                        retryAction: { viewStore.send(.fetchProducts) }
+                    )
+                    
+                } else {
+                    List {
+                        ForEachStore(
+                            self.store.scope(
+                                state: \.productListState,
+                                action: ProductListDomain.Action
+                                    .product(id: action:)
+                            )
+                        ) {
+                            ProductCell(store: $0)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ProductListViewDuplication_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductListViewDuplication(
+            store: Store(
+                initialState: ProductListDomain.State(),
+                reducer: ProductListDomain.reducer,
+                environment: ProductListDomain.Environment(
+                    fetchProducts: { Product.sample },
+                    sendOrder: { _ in "OK" },
+                    uuid: { UUID() }
+                )
+            )
+        )
+    }
+}
