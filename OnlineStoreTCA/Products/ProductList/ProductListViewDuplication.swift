abec90b99c6c6a2718996f3b3dc55a7f8a881a16
Daniel Torres <danstorre@gmail.com>
2023-01-25 08:17:19 -0400

    `ProductListViewDuplication` now sends a task to fetch products.

--- OnlineStoreTCA/Products/ProductList/ProductListViewDuplication.swift
+++ OnlineStoreTCA/Products/ProductList/ProductListViewDuplication.swift
@@ -37,6 +37,9 @@ struct ProductListViewDuplication: View {
                     }
                 }
             }
+            .task {
+                viewStore.send(.fetchProducts)
+            }
         }
     }
 }
